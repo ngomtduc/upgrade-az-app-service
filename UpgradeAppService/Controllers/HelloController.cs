29d2054da139e33e1ab054103b2cc1b58cc03a7d
ThienNguyenTriBuu <thien.nguyentribuu@petronas.com>
2023-12-01 00:21:17 +0700

    add sample net 6

---
+++ UpgradeAppService/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UpgradeAppService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello. This API runs on Net 6.";
+        }
+    }
+}
